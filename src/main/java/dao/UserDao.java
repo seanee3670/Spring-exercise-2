58f4ec42bc51d0f56e7b389fe850492601aa5e08
seanee3670 <cseok3670@gmail.com>
2022-10-24 15:11:39 +0900

    [init] 1024 Spring exercise - introduction of JdbcTemplate

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -2,74 +2,62 @@ package dao;
 
 
 import domain.User;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import javax.sql.DataSource;
 import java.sql.*;
+import java.util.List;
 import java.util.Map;
 
 public class UserDao {
+    private JdbcTemplate jdbcTemplate;
 
-    private ConnectionMaker cm;
-    public UserDao() {
-        this.cm = new AwsConnectionMaker();
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public UserDao(ConnectionMaker cm) {
-        this.cm = cm;
-    }
-
-    public void add(User user) {
-        Map<String, String> env = System.getenv();
-        try {
-            // DB접속 (ex sql workbeanch실행)
-            Connection c = cm.makeConnection();
-
-            // Query문 작성
-            PreparedStatement pstmt = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
-            pstmt.setString(1, user.getId());
-            pstmt.setString(2, user.getName());
-            pstmt.setString(3, user.getPassword());
-
-            // Query문 실행
-            pstmt.executeUpdate();
-
-            pstmt.close();
-            c.close();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public void add(final User user) throws SQLException {
+        this.jdbcTemplate.update("INSERT INTO users(id, name, password) VALUES (?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword());
     }
 
     public User findById(String id) {
-        Map<String, String> env = System.getenv();
-        Connection c;
         try {
-            // DB접속 (ex sql workbeanch실행)
-            c = cm.makeConnection();
+            return this.jdbcTemplate.queryForObject("SELECT * From users WHERE USERS.ID = ?", new RowMapper<User>() {
+                @Override
+                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    System.out.println(rowNum);
+                    return new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
 
-            // Query문 작성
-            PreparedStatement pstmt = c.prepareStatement("SELECT * FROM users WHERE id = ?");
-            pstmt.setString(1, id);
-
-            // Query문 실행
-            ResultSet rs = pstmt.executeQuery();
-            rs.next();
-            User user = new User(rs.getString("id"), rs.getString("name"),
-                    rs.getString("password"));
-
-            rs.close();
-            pstmt.close();
-            c.close();
-
-            return user;
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+                }
+            }, id);
+        } catch (DataAccessException e) {
+            return null;
         }
     }
 
+    public List<User> getAll() {
+        return this.jdbcTemplate.query("SELECT * FROM users ORDER BY id", new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+                return user;
+            }
+        });
+    }
+
+    public int getCountAll() throws SQLException {
+        return this.jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users",Integer.class);
+    }
+
+    public void deleteAll() throws SQLException{
+        this.jdbcTemplate.update("DELETE FROM users");
+    }
+
     public static void main(String[] args) {
-        UserDao userDao = new UserDao();
+        UserDao userDao = new UserDao(new JdbcTemplate());
 //        userDao.add();
         User user = userDao.findById("6");
         System.out.println(user.getName());
--- src/main/java/dao/UserDaoFactory.java
+++ src/main/java/dao/UserDaoFactory.java
@@ -3,12 +3,35 @@ package dao;
 import domain.User;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sound.midi.Soundbank;
+import javax.sql.DataSource;
+import java.util.Map;
 
 @Configurable
 public class UserDaoFactory {
 
     @Bean
-    public UserDao awsUserDao() {
-        return new UserDao(new AwsConnectionMaker());
+    public UserDao awsUserDao(JdbcTemplate jdbcTemplate) {
+        return new UserDao(jdbcTemplate);
     }
+
+    @Bean
+    DataSource dataSource() {
+        Map<String, String> env = System.getenv();
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl(env.get("DB_HOST"));
+        dataSource.setUsername(env.get("DB_USER"));
+        dataSource.setPassword(env.get("DB_PASSWORD"));
+        return dataSource;
+    }
+
+    @Bean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
 }
